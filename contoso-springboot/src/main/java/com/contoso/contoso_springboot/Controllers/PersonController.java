d7a93e917ba78b0f817ec173aad98a1ba406b0ab
Miguel Esteban Parrado Lopez <miguel.parrado@globant.com>
2024-09-03 23:01:01 -0500

    contoso-project: Update person

--- contoso-springboot/src/main/java/com/contoso/contoso_springboot/Controllers/PersonController.java
+++ contoso-springboot/src/main/java/com/contoso/contoso_springboot/Controllers/PersonController.java
@@ -1,12 +1,12 @@
 package com.contoso.contoso_springboot.Controllers;
 import com.contoso.contoso_springboot.Models.Person;
 import com.contoso.contoso_springboot.Repositories.PersonRepository;
-import org.apache.tomcat.jni.Pool;
+import com.contoso.contoso_springboot.Services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/person")
@@ -14,10 +14,37 @@ public class PersonController {
 
     @Autowired
     private PersonRepository personRepository;
+    @Autowired
+    private PersonService personService;
 
     @PostMapping("/addPerson")
     public void addPerson(@RequestBody Person person){
         personRepository.save(person);
+    }
+
+    @GetMapping("/persons")
+    public List<Person> getPersons(){
+        return personService.getPersons();
+    }
+
+    @GetMapping("/person/{id}")
+    public Optional<Person> getPersonById(@PathVariable Long id){
+        return personService.getPersonById(id);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deletePerson(@PathVariable Long id){
+        personService.deletePerson(id);
+    }
+
+    @PostMapping("/save")
+    public void savePerson(@RequestBody Person person){
+        personService.savePerson(person);
+    }
+
+    @PostMapping("/update")
+    public void updatePerson(@RequestBody Person person){
+        personService.savePerson(person);
     }
 
 }
